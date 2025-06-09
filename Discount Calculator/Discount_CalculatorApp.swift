f800b6812c0e25be9e9b67d67a2ed101da45851d
Kyle Schang <kyle@schang.net>
2025-06-08 22:27:03 -0400

    Initial Commit

---
+++ Discount Calculator/Discount_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discount_CalculatorApp.swift
+//  Discount Calculator
+//
+//  Created by Kyle Schang on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Discount_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
