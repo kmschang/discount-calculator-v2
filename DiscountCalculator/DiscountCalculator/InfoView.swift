ec44e41d3dfc96657bcf7ae6148f562379e13238
Kyle Schang <kyle@schang.net>
2026-03-23 18:53:57 -0400

    updaded the X to be a checkmark in the info view

--- DiscountCalculator/DiscountCalculator/InfoView.swift
+++ DiscountCalculator/DiscountCalculator/InfoView.swift
@@ -91,7 +91,7 @@ struct InfoView: View {
                     Button {
                         dismiss()
                     } label: {
-                        Image(systemName: "xmark")
+                        Image(systemName: "checkmark")
                     }
                     .tint(.primary)
                 }
